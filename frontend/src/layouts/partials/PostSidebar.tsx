e04a3f70e41776b2d4987118a2fd69b8259da0e1
Jay Shitre <jayshitre@gmail.com>
2024-01-14 02:01:14 +0530

    Fixed no-children-prop bug

--- frontend/src/layouts/partials/PostSidebar.tsx
+++ frontend/src/layouts/partials/PostSidebar.tsx
@@ -27,85 +27,6 @@ const PostSidebar = ({
     "Above_2000": false
   })
 
-  const DateFilter = (
-  <div className="flex flex-wrap flex-row gap-4 mb-6">
-    <button
-      className={`btn ${date.today ? 'btn-primary': 'btn-outline-primary'} px-4 lg:inline-flex items-center cursor-pointer`}
-      onClick={() => setDate({
-        ...date,
-        today: !date.today
-        })}
-    >
-      Today
-    </button>
-
-    <button
-      className={`btn ${date.tomorrow ? 'btn-primary': 'btn-outline-primary'} px-4 lg:inline-flex items-center cursor-pointer`}
-      onClick={() => setDate({
-        ...date,
-        tomorrow: !date.tomorrow
-        })}
-    >
-      Tomorrow
-    </button>
-
-    <button
-      className={`btn ${date.weekend ? 'btn-primary': 'btn-outline-primary'} px-4 lg:inline-flex items-center cursor-pointer`}
-      onClick={() => setDate({
-        ...date,
-        weekend: !date.weekend
-        })}
-    >
-      Weekend
-    </button>
-  </div>
-  )
-
-  const PriceFilter = (
-  <div className="flex flex-wrap flex-row gap-4 mb-6">
-    <button
-      className={`btn ${price.Free ? 'btn-primary': 'btn-outline-primary'} px-4 lg:inline-flex items-center cursor-pointer`}
-      onClick={() => setPrice({
-        ...price,
-        Free: !price.Free
-        })}
-    >
-      Free
-    </button>
-
-    <button
-      className={`btn ${price.below_500 ? 'btn-primary': 'btn-outline-primary'} px-4 lg:inline-flex items-center cursor-pointer`}
-      onClick={() => setPrice({
-        ...price,
-        below_500: !price.below_500
-        })}
-    >
-      1-500
-    </button>
-
-    <button
-      className={`btn ${price.between_500_1000 ? 'btn-primary': 'btn-outline-primary'} px-4 lg:inline-flex items-center cursor-pointer`}
-      onClick={() => setPrice({
-        ...price,
-        between_500_1000: !price.between_500_1000
-        })}
-    >
-      501-1000
-    </button>
-
-    <button
-      className={`btn ${price.Above_2000 ? 'btn-primary': 'btn-outline-primary'} px-4 lg:inline-flex items-center cursor-pointer`}
-      onClick={() => setPrice({
-        ...price,
-        Above_2000: !price.Above_2000
-        })}
-    >
-      Above 2000
-    </button>
-  </div>
-  )
-
-
   return (
     <div className="lg:col-4">
       {/* <!-- categories --> */}
@@ -135,13 +56,85 @@ const PostSidebar = ({
       <h5 className="mb-6">Filters</h5>
       <Accordion
         title={"Date"}
-        children={DateFilter}
-      />
+      >
+        <div className="flex flex-wrap flex-row gap-4 mb-6">
+          <button
+            className={`btn ${date.today ? 'btn-primary': 'btn-outline-primary'} px-4 lg:inline-flex items-center cursor-pointer`}
+            onClick={() => setDate({
+              ...date,
+              today: !date.today
+              })}
+          >
+            Today
+          </button>
+
+          <button
+            className={`btn ${date.tomorrow ? 'btn-primary': 'btn-outline-primary'} px-4 lg:inline-flex items-center cursor-pointer`}
+            onClick={() => setDate({
+              ...date,
+              tomorrow: !date.tomorrow
+              })}
+          >
+            Tomorrow
+          </button>
+
+          <button
+            className={`btn ${date.weekend ? 'btn-primary': 'btn-outline-primary'} px-4 lg:inline-flex items-center cursor-pointer`}
+            onClick={() => setDate({
+              ...date,
+              weekend: !date.weekend
+              })}
+          >
+            Weekend
+          </button>
+        </div>
+      </Accordion>
 
       <Accordion
         title={"Price"}
-        children={PriceFilter}
-      />
+      >
+        <div className="flex flex-wrap flex-row gap-4 mb-6">
+          <button
+            className={`btn ${price.Free ? 'btn-primary': 'btn-outline-primary'} px-4 lg:inline-flex items-center cursor-pointer`}
+            onClick={() => setPrice({
+              ...price,
+              Free: !price.Free
+              })}
+          >
+            Free
+          </button>
+
+          <button
+            className={`btn ${price.below_500 ? 'btn-primary': 'btn-outline-primary'} px-4 lg:inline-flex items-center cursor-pointer`}
+            onClick={() => setPrice({
+              ...price,
+              below_500: !price.below_500
+              })}
+          >
+            1-500
+          </button>
+
+          <button
+            className={`btn ${price.between_500_1000 ? 'btn-primary': 'btn-outline-primary'} px-4 lg:inline-flex items-center cursor-pointer`}
+            onClick={() => setPrice({
+              ...price,
+              between_500_1000: !price.between_500_1000
+              })}
+          >
+            501-1000
+          </button>
+
+          <button
+            className={`btn ${price.Above_2000 ? 'btn-primary': 'btn-outline-primary'} px-4 lg:inline-flex items-center cursor-pointer`}
+            onClick={() => setPrice({
+              ...price,
+              Above_2000: !price.Above_2000
+              })}
+          >
+            Above 2000
+          </button>
+        </div>
+      </Accordion>
 
       {/* <!-- tags --> */}
       {/*<div className="mb-8">
