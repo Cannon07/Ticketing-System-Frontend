a5df4c370ced53d4a1dd58f9414356359d0f8296
Nikhil <nikhildmagar@gmail.com>
2024-01-16 17:22:21 +0530

    <hr/> added

--- frontend/src/layouts/components/EventPostPage.tsx
+++ frontend/src/layouts/components/EventPostPage.tsx
@@ -36,12 +36,12 @@ const EventPostPage = ({ similarPosts }: any) => {
 
 
 
-    
+
     const handleScroll = () => {
-        console.log("scroll"+containerRef?.current?.scrollWidth)
-        console.log("client"+containerRef?.current?.clientWidth)
-        console.log("left"+containerRef.current?.scrollLeft);
-        console.log("diff"+(Number(containerRef?.current?.scrollWidth)-Number(containerRef?.current?.clientWidth)))
+        // console.log("scroll"+containerRef?.current?.scrollWidth)
+        // console.log("client"+containerRef?.current?.clientWidth)
+        // console.log("left"+containerRef.current?.scrollLeft);
+        // console.log("diff"+(Number(containerRef?.current?.scrollWidth)-Number(containerRef?.current?.clientWidth)))
         if (containerRef.current) {
             setShowLeftArrow(containerRef.current.scrollLeft > 0);
             setShowRightArrow(containerRef.current.scrollLeft < containerRef.current.scrollWidth - containerRef.current.clientWidth);
@@ -50,23 +50,23 @@ const EventPostPage = ({ similarPosts }: any) => {
     };
 
     const scrollLeft = () => {
-       
+
         if (containerRef.current) {
-            containerRef.current.scrollLeft -= 100; 
+            containerRef.current.scrollLeft -= 100;
         }
     };
 
     const scrollRight = () => {
 
         console.log(containerRef.current?.scrollLeft)
-    
+
         if (containerRef.current) {
-            containerRef.current.scrollLeft += 100; 
+            containerRef.current.scrollLeft += 100;
         }
     };
 
     useEffect(() => {
-       
+
         handleScroll();
 
         const container = containerRef.current;
@@ -284,6 +284,8 @@ const EventPostPage = ({ similarPosts }: any) => {
                         </div>
                     </div> */}
                     <div>
+                        <hr className="h-px my-8 dark:bg-gray-600 border-0 bg-gray-200"/>
+
                         <h2 className="h3">Artists</h2>
                         <div className="relative">
                             <div className="flex items-center gap-6 overflow-scroll no-scrollbar" ref={containerRef}>
@@ -307,15 +309,18 @@ const EventPostPage = ({ similarPosts }: any) => {
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
                                 </svg>
-                              
+
                             </button>
                             <button className={`absolute right-0 top-1/2 transform -translate-y-1/2 ${showRightArrow ? '' : 'hidden'}`} onClick={scrollRight}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                                 </svg>
-                             
+
                             </button>
                         </div>
+
+                        <hr className="h-px my-8 dark:bg-gray-600 border-0 bg-gray-200"/>
+
 
                     </div>
                     {/* <Disqus className="mt-20" /> */}
