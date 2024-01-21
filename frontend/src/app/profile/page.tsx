45bb15ec679ecbc2c0004736ee1e12c2a64f4365
Nikhil <nikhildmagar@gmail.com>
2024-01-21 15:25:57 +0530

    modifications in user-ticket

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -2,8 +2,8 @@ import UserTicket from "@/components/UserTicket"
 
 const UserProfile = () => {
 
-    const numsTop = ['10px','20px','30px','40px','40px']
-    const numsBottom = [0,3]
+    const numsTop = ['10px', '20px', '30px', '40px', '40px']
+    const numsBottom = [0, 3]
 
     return (
         <div className="section-sm">
@@ -13,13 +13,17 @@ const UserProfile = () => {
                     <div className="grid grid-cols-3">
 
                         <div></div>
-                        <div className="col-span-3 md:col-span-2 md:flex md:flex-wrap lg:col-span-2 ">
+                        <div className="col-span-3 lg:col-span-2">
+                            <div className="flex justify-center items-center flex-wrap">
+                      
+                            <UserTicket />
+                            <UserTicket />
+                            <UserTicket />
+                            <UserTicket />
+                            <UserTicket />
+ 
+                            </div>
 
-                        <UserTicket />
-                        <UserTicket/>
-                        <UserTicket/>
-                        <UserTicket/>
-                        <UserTicket/>
 
 
                         </div>
