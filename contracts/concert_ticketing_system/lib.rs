68a676d09247ddaf8c9d4fb630210de252a770e1
Jay Shitre <jayshitre@gmail.com>
2024-01-30 19:50:25 +0530

    Added update methods for user and organizer

--- contracts/concert_ticketing_system/lib.rs
+++ contracts/concert_ticketing_system/lib.rs
@@ -13,17 +13,20 @@ mod concert_ticketing_system {
     }
 
     #[ink(event)]
-    pub struct AccountRegistered {
+    pub struct AccountModified {
         #[ink(topic)]
         account: Option<AccountId>,
         data: String,
     } 
 
+
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
         AlreadyRegisteredAsUser,
+        NotRegisteredAsUser,
         AlreadyRegisteredAsOrganizer,
+        NotRegisteredAsOrganizer,
     }
 
     pub type Result<T> = core::result::Result<T, Error>;
@@ -50,7 +53,25 @@ mod concert_ticketing_system {
 
             self.users.insert(caller, &data_hash); 
             
-            Self::env().emit_event(AccountRegistered {
+            Self::env().emit_event(AccountModified {
+                account: Some(caller),
+                data: data_hash,
+            });
+            
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn update_user(&mut self, data_hash: String) -> Result<()> {
+            let caller = self.env().caller();
+
+            if !self.users.contains(caller) {
+                return Err(Error::NotRegisteredAsUser)
+            } 
+
+            self.users.insert(caller, &data_hash); 
+            
+            Self::env().emit_event(AccountModified {
                 account: Some(caller),
                 data: data_hash,
             });
@@ -77,11 +98,29 @@ mod concert_ticketing_system {
 
             self.organizers.insert(caller, &data_hash); 
 
-            Self::env().emit_event(AccountRegistered {
+            Self::env().emit_event(AccountModified {
                 account: Some(caller),
                 data: data_hash,
             });
 
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn update_organizer(&mut self, data_hash: String) -> Result<()> {
+            let caller = self.env().caller();
+
+            if !self.organizers.contains(caller) {
+                return Err(Error::NotRegisteredAsOrganizer)
+            } 
+
+            self.organizers.insert(caller, &data_hash); 
+            
+            Self::env().emit_event(AccountModified {
+                account: Some(caller),
+                data: data_hash,
+            });
+            
             Ok(())
         }
         
@@ -101,11 +140,11 @@ mod concert_ticketing_system {
         fn assert_event(event: &ink::env::test::EmittedEvent, expected_from: Option<AccountId>, expected_data: String) {
             let decoded_event = <Event as scale::Decode>::decode(&mut &event.data[..])
                 .expect("encountered invalid contract event data buffer");
-            if let Event::AccountRegistered(AccountRegistered { account, data }) = decoded_event {
-                assert_eq!(account, expected_from, "encountered invalid AccountRegistered.user");
-                assert_eq!(data, expected_data, "encountered invalid AccountRegistered.data");
+            if let Event::AccountModified(AccountModified { account, data }) = decoded_event {
+                assert_eq!(account, expected_from, "encountered invalid AccountModified.user");
+                assert_eq!(data, expected_data, "encountered invalid AccountModified.data");
             } else {
-                panic!("encountered unexpected event kind: expected a AccountRegistered event")
+                panic!("encountered unexpected event kind: expected a AccountModified event")
             }
         }
 
@@ -145,6 +184,53 @@ mod concert_ticketing_system {
         } 
 
         #[ink::test]
+        fn update_user_works() {
+            let mut contract = ConcertTicketingSystem::new(); 
+
+            assert_eq!(contract.register_user("Gekki".to_string()), Ok(()));
+            assert_eq!(contract.get_user().unwrap(), "Gekki".to_string());
+            
+            let emitted_events = ink::env::test::recorded_events().collect::<Vec<_>>();
+            assert_eq!(1, emitted_events.len());
+            assert_event(
+                &emitted_events[0],
+                Some(AccountId::from([0x01; 32])),
+                "Gekki".to_string(),
+            );
+
+            assert_eq!(contract.update_user("Hello".to_string()), Ok(()));
+            assert_eq!(contract.get_user().unwrap(), "Hello".to_string());
+            
+            let emitted_events = ink::env::test::recorded_events().collect::<Vec<_>>();
+            assert_eq!(2, emitted_events.len());
+            assert_event(
+                &emitted_events[1],
+                Some(AccountId::from([0x01; 32])),
+                "Hello".to_string(),
+            );
+        }
+
+        #[ink::test]
+        fn update_user_fails() {
+            let mut contract = ConcertTicketingSystem::new(); 
+
+            assert_eq!(contract.update_user("Hello".to_string()), Err(Error::NotRegisteredAsUser));
+
+            assert_eq!(contract.register_organizer("Gekki".to_string()), Ok(()));
+            assert_eq!(contract.get_organizer().unwrap(), "Gekki".to_string());
+            
+            let emitted_events = ink::env::test::recorded_events().collect::<Vec<_>>();
+            assert_eq!(1, emitted_events.len());
+            assert_event(
+                &emitted_events[0],
+                Some(AccountId::from([0x01; 32])),
+                "Gekki".to_string(),
+            );
+
+            assert_eq!(contract.update_user("Hello".to_string()), Err(Error::NotRegisteredAsUser));
+        }
+
+        #[ink::test]
         fn register_organizer_works() {
             let mut contract = ConcertTicketingSystem::new(); 
 
@@ -177,6 +263,53 @@ mod concert_ticketing_system {
             
             assert_eq!(contract.register_user("Hello".to_string()), Err(Error::AlreadyRegisteredAsUser));
             assert_eq!(contract.register_organizer("Hello".to_string()), Err(Error::AlreadyRegisteredAsUser));
+        }
+
+        #[ink::test]
+        fn update_organizer_works() {
+            let mut contract = ConcertTicketingSystem::new(); 
+
+            assert_eq!(contract.register_organizer("Gekki".to_string()), Ok(()));
+            assert_eq!(contract.get_organizer().unwrap(), "Gekki".to_string());
+            
+            let emitted_events = ink::env::test::recorded_events().collect::<Vec<_>>();
+            assert_eq!(1, emitted_events.len());
+            assert_event(
+                &emitted_events[0],
+                Some(AccountId::from([0x01; 32])),
+                "Gekki".to_string(),
+            );
+
+            assert_eq!(contract.update_organizer("Hello".to_string()), Ok(()));
+            assert_eq!(contract.get_organizer().unwrap(), "Hello".to_string());
+            
+            let emitted_events = ink::env::test::recorded_events().collect::<Vec<_>>();
+            assert_eq!(2, emitted_events.len());
+            assert_event(
+                &emitted_events[1],
+                Some(AccountId::from([0x01; 32])),
+                "Hello".to_string(),
+            );
+        }
+
+        #[ink::test]
+        fn update_organizer_fails() {
+            let mut contract = ConcertTicketingSystem::new(); 
+
+            assert_eq!(contract.update_organizer("Hello".to_string()), Err(Error::NotRegisteredAsOrganizer));
+
+            assert_eq!(contract.register_user("Gekki".to_string()), Ok(()));
+            assert_eq!(contract.get_user().unwrap(), "Gekki".to_string());
+            
+            let emitted_events = ink::env::test::recorded_events().collect::<Vec<_>>();
+            assert_eq!(1, emitted_events.len());
+            assert_event(
+                &emitted_events[0],
+                Some(AccountId::from([0x01; 32])),
+                "Gekki".to_string(),
+            );
+
+            assert_eq!(contract.update_organizer("Hello".to_string()), Err(Error::NotRegisteredAsOrganizer));
         }
     }
 }
